4dc2dde1817b5d36486f79d27b4ceb5cc8f9d59d
karam nasser <knasser051@hotmail.com>
2021-10-19 15:00:13 -0500

    setup repository

---
+++ arrays.js
@@ -0,0 +1,51 @@
+//////////////////PROBLEM 1////////////////////
+/* Create a copy of the faveColors array called 'colorCopy' using the slice method. */
+
+const faveColors = ['red', 'green', 'black']
+
+const colorCopy = faveColors.slice;
+
+
+//////////////////PROBLEM 2////////////////////
+/* Add a fourth color to the end of the 'colorCopy' array using the push method. */
+
+const count = colorCopy.push('white');
+console.log(colorCopy);
+
+//////////////////PROBLEM 3////////////////////
+/*
+  Using an array method, create a new array called 'middleNums' from the 
+  numbers array that will capture only the middle numbers (2, 3, 4).
+*/
+
+const numbers = [1, 2, 3, 4, 5]
+
+const middleNums = numbers.splice(2, 3, 4);
+console.log(middleNums);
+
+
+//////////////////PROBLEM 4////////////////////
+/* 
+  Create a function called 'bigOrSmall' that takes in one parameter, 'arr', 
+  which will be an array of numbers. 
+  Inside of the bigOrSmall function, create a new array called 'answers'. 
+  Then, loop over the passed in arr parameter, and check to see if the number in the
+  array is GREATER than 100. 
+  If it is, push 'big' as a string to the answers array. 
+  If the number is LESS than or EQUAL to 100, push 'small' as a string 
+  to the answers array. 
+  Return the answers array inside of the function.
+*/
+
+function bigOrSmall(arr){
+let arr = numbers;
+let array = answers;
+}
+for (let arr = 0; arr < 100; arr++){
+  if (arr > 100);
+  console.log('big');
+  if (arr <= 100);
+  console.log('small');
+}
+
+
---
+++ sldc.js
@@ -0,0 +1,29 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Analysis of Requirements
+//Planning
+// Design
+// Implementation
+//Testing & Integration
+// Maintain
+
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle 
+    and set the values of each variable to a definition of the step.
+*/
+{
+let 1 = "Analysis of Requirements";
+let 2 = "Planning";
+let 3 = "Design";
+let 4 = "Implementation";
+let 5 = "Testing & Integration";
+let 6 = "Maintain";
+}
